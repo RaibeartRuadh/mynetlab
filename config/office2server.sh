2bb367ac9f0a223bf91ab4b278c808370968c27c
RaibeartRuadh <38784332+RaibeartRuadh@users.noreply.github.com>
2020-12-04 14:42:45 +0300

    Delete office2server.sh

--- config/office2server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "DEFROUTE=no" >> /etc/sysconfig/network-scripts/ifcfg-eth0 
-echo "GATEWAY=192.168.1.129" >> /etc/sysconfig/network-scripts/ifcfg-eth1
-systemctl restart network
-yum install mc net-tools -y
