55cc88e075ada688a8e7073c9c5bf6a677f91eae
RaibeartRuadh <38784332+RaibeartRuadh@users.noreply.github.com>
2020-12-04 14:43:08 +0300

    Delete office1router.sh

--- config/office1router.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sysctl net.ipv4.conf.all.forwarding=1
-echo "net.ipv4.conf.all.forwarding=1" > /etc/sysctl.d/net.conf
-echo "DEFROUTE=no" >> /etc/sysconfig/network-scripts/ifcfg-eth0 
-echo "GATEWAY=192.168.255.5" >> /etc/sysconfig/network-scripts/ifcfg-eth1
-systemctl restart network
-yum install mc net-tools -y
